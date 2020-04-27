2aaee95daa47f44dd35eeef718d14190d8ace9a9
松島勇貴 <macho.macho.dev@gmail.com>
2020-04-27 09:58:34 +0900

    Add PlainRecordView

---
+++ lib/view/record/plainRecordView.dart
@@ -0,0 +1,235 @@
+
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+import 'package:raisingchildrenrecord2/l10n/l10n.dart';
+import 'package:raisingchildrenrecord2/model/baby.dart';
+import 'package:raisingchildrenrecord2/model/record.dart';
+import 'package:raisingchildrenrecord2/model/user.dart';
+import 'package:raisingchildrenrecord2/viewmodel/record/plainRecordViewModel.dart';
+import 'package:intl/intl.dart';
+
+class PlainRecordView extends StatefulWidget {
+  Record record;
+  User user;
+  Baby baby;
+  bool isNew;
+
+  PlainRecordView({ Key key, this.record, this.user, this.baby, this.isNew }): super(key: key);
+
+  @override
+  _PlainRecordViewState createState() => _PlainRecordViewState();
+}
+
+class _PlainRecordViewState extends State<PlainRecordView> {
+  @override
+  Widget build(BuildContext context) {
+    return Provider<PlainRecordViewModel>(
+      create: (_) => PlainRecordViewModel(widget.record, widget.user, widget.baby, L10n.of(context)),
+      child: _RecordScaffold(record: widget.record, isNew: widget.isNew),
+    );
+  }
+}
+
+class _RecordScaffold extends StatefulWidget {
+  Record record;
+  bool isNew;
+
+  _RecordScaffold({ Key key, this.record, this.isNew }): super(key: key);
+
+  @override
+  _RecordScaffoldState createState() => _RecordScaffoldState();
+}
+
+class _RecordScaffoldState extends State<_RecordScaffold> {
+
+  PlainRecordViewModel _viewModel;
+
+  @override
+  void initState() {
+    super.initState();
+    _viewModel = Provider.of<PlainRecordViewModel>(context, listen: false);
+    _viewModel.onSaveComplete.listen((_) => _pop());
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    L10n l10n = L10n.of(context);
+    return Scaffold(
+      appBar: AppBar(
+          title: Text(widget.isNew ?? false ? l10n.recordTitleNew : l10n.recordTitleEdit),
+          actions: <Widget>[
+            IconButton(
+              icon: Icon(Icons.check),
+              onPressed: () => _viewModel.onSaveButtonTapped.add(null),
+            ),
+          ]
+      ),
+      body: _RecordForm(isNew: widget.isNew),
+    );
+  }
+
+  void _pop() {
+    Navigator.pop(context);
+  }
+}
+
+class _RecordForm extends StatefulWidget {
+  bool isNew;
+
+  _RecordForm({ Key key, this.isNew }): super(key: key);
+  @override
+  _RecordFormState createState() => _RecordFormState();
+}
+
+class _RecordFormState extends State<_RecordForm> {
+  final _recordTypeFont = const TextStyle(fontSize: 24.0, fontWeight: FontWeight.bold);
+  final _dateButtonFont = const TextStyle(color: Colors.blue, fontSize: 20.0);
+  final _deleteButtonFont = const TextStyle(color: Colors.red, fontSize: 20.0);
+  final _listItemFont = const TextStyle(fontSize: 20.0);
+  final _dateFormat = DateFormat().add_yMd().add_Hms();
+
+  TextEditingController _noteController;
+  PlainRecordViewModel _viewModel;
+
+  @override
+  void initState() {
+    super.initState();
+    _noteController = TextEditingController();
+    _viewModel = Provider.of<PlainRecordViewModel>(context, listen: false);
+    _viewModel.note.listen((note) {
+      _noteController.text = note;
+    });
+  }
+
+  @override
+  void dispose() {
+    super.dispose();
+    _viewModel.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    L10n l10n = L10n.of(context);
+    return Container(
+      padding: EdgeInsets.fromLTRB(24, 36, 24, 36),
+      child: Column(
+        children: <Widget>[
+          Row(
+            children: <Widget>[
+              StreamBuilder(
+                  stream: _viewModel.assetName,
+                  builder: (context, snapshot) {
+                    if (!snapshot.hasData) {
+                      return Container();
+                    }
+                    return Container(
+                      decoration: BoxDecoration(
+                        shape: BoxShape.circle,
+                        image: DecorationImage(
+                          fit: BoxFit.fitHeight,
+                          image: AssetImage(snapshot.data),
+                        ),
+                      ),
+                      height: 48,
+                      width: 48,
+                    );
+                  }
+              ),
+              Expanded(
+                child: StreamBuilder(
+                  stream: _viewModel.title,
+                  builder: (context, snapshot) {
+                    if (!snapshot.hasData) {
+                      return Container();
+                    }
+                    return Container(
+                      padding: EdgeInsets.fromLTRB(12, 0, 0, 0),
+                      child: Text(
+                        snapshot.data,
+                        style: _recordTypeFont,
+                      ),
+                    );
+                  },
+                ),
+              ),
+            ],
+            mainAxisAlignment: MainAxisAlignment.start,
+          ),
+          Container(
+            height: 36,
+          ),
+          StreamBuilder(
+            stream: _viewModel.dateTime,
+            builder: (context, snapshot) {
+              final dateTime = snapshot.data ?? DateTime.now();
+              return Container(
+                alignment: Alignment.centerLeft,
+                child: FlatButton(
+                  child: Text(
+                    _dateFormat.format(dateTime),
+                    style: _dateButtonFont,
+                  ),
+                  padding: EdgeInsets.fromLTRB(0, 0, 0, 0),
+                  onPressed: () => _onDateTimeButtonPressed(dateTime),
+                ),
+              );
+            },
+          ),
+          Container(
+            height: 14,
+          ),
+          TextField(
+            decoration: InputDecoration(
+              border: OutlineInputBorder(),
+              labelText: l10n.recordLabelNote,
+            ),
+            onChanged: (text) => _viewModel.onNoteChanged.add(text),
+            controller: _noteController,
+          ),
+          Container(
+            height: 24,
+          ),
+          !(widget.isNew ?? false) ? FlatButton(
+            child: Text(
+              l10n.recordDeleteButtonLabel,
+              style: _deleteButtonFont,
+            ),
+            onPressed: () => _viewModel.onDeleteButtonTapped.add(null),
+          ) : Container()
+        ],
+      ),
+    );
+  }
+
+  void _onDateTimeButtonPressed(DateTime currentDateTime) async {
+    DateTime selectedDate = await showDatePicker(
+      context: context,
+      initialDate: currentDateTime,
+      firstDate: DateTime(2018),
+      lastDate: DateTime(2030),
+      builder: (BuildContext context, Widget child) {
+        return child;
+      },
+    );
+
+    if (selectedDate == null) {
+      return;
+    }
+
+    TimeOfDay selectedTime = await showTimePicker(
+      context: context,
+      initialTime: TimeOfDay.fromDateTime(currentDateTime),
+      builder: (BuildContext context, Widget child) {
+        return child;
+      },
+    );
+
+    if (selectedTime == null) {
+      return;
+    }
+
+    DateTime selectedDateTime = DateTime(selectedDate.year, selectedDate.month, selectedDate.day, selectedTime.hour, selectedTime.minute);
+    _viewModel.onDateTimeSelected.add(selectedDateTime);
+  }
+}
---
+++ lib/viewmodel/record/plainRecordViewModel.dart
@@ -0,0 +1,102 @@
+
+import 'dart:async';
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:raisingchildrenrecord2/l10n/l10n.dart';
+import 'package:raisingchildrenrecord2/model/baby.dart';
+import 'package:raisingchildrenrecord2/model/record.dart';
+import 'package:raisingchildrenrecord2/model/user.dart';
+import 'package:rxdart/rxdart.dart';
+
+class PlainRecordViewModel {
+  BehaviorSubject<Record> recordBehaviorSubject;
+  User user;
+  Baby baby;
+  L10n l10n;
+
+  Stream<String> get assetName => recordBehaviorSubject.stream.map((record) => record.assetName);
+  Stream<String> get title => recordBehaviorSubject.stream.map((record) => record.typeName(l10n));
+
+  final StreamController<void> _onSaveButtonTappedStreamController = StreamController<void>();
+  StreamSink<void> get onSaveButtonTapped => _onSaveButtonTappedStreamController.sink;
+
+  Stream<DateTime> get dateTime => recordBehaviorSubject.stream.map((record) => record.dateTime);
+  final StreamController<DateTime> _onDateTimeSelectedStreamController = StreamController<DateTime>();
+  StreamSink<DateTime> get onDateTimeSelected => _onDateTimeSelectedStreamController.sink;
+
+  Stream<String> get note => recordBehaviorSubject.stream.map((record) => record.note);
+  final StreamController<String> _onNoteChangedStreamController = StreamController<String>();
+  StreamSink<String> get onNoteChanged => _onNoteChangedStreamController.sink;
+
+  final StreamController<void> _onSaveCompleteStreamController = StreamController<void>();
+  Stream<void> get onSaveComplete => _onSaveCompleteStreamController.stream;
+
+  final StreamController<void> _onDeleteButtonTappedStreamController = StreamController<void>();
+  StreamSink<void> get onDeleteButtonTapped => _onDeleteButtonTappedStreamController.sink;
+
+  PlainRecordViewModel(record, this.user, this.baby, this.l10n) {
+    print("### record.note: ${record.note}");
+    recordBehaviorSubject = BehaviorSubject.seeded(record);
+
+    _onDateTimeSelectedStreamController.stream.listen((date) {
+      Record record = recordBehaviorSubject.value;
+      record.dateTime = date;
+      recordBehaviorSubject.add(record);
+    });
+
+    _onNoteChangedStreamController.stream.listen((note) {
+      Record record = recordBehaviorSubject.value;
+      record.note = note;
+      recordBehaviorSubject.add(record);
+    });
+
+    CombineLatestStream.combine2(
+        recordBehaviorSubject,
+        _onSaveButtonTappedStreamController.stream,
+        (record, _) => record
+    )
+    .listen((record) => _save(record));
+
+    CombineLatestStream.combine2(
+        recordBehaviorSubject,
+        _onDeleteButtonTappedStreamController.stream,
+        (record, _) => record
+    )
+    .listen((record) => _delete(record));
+  }
+
+  void _save(Record record) async {
+    print("### save $record");
+    Firestore.instance
+        .collection('families')
+        .document(user.familyId)
+        .collection("babies")
+        .document(baby.id)
+        .collection("records")
+        .document(record.id)
+        .setData(record.map);
+
+    _onSaveCompleteStreamController.sink.add(null);
+  }
+
+  void _delete(Record record) async {
+    Firestore.instance
+        .collection('families')
+        .document(user.familyId)
+        .collection("babies")
+        .document(baby.id)
+        .collection("records")
+        .document(record.id)
+        .delete();
+
+    _onSaveCompleteStreamController.sink.add(null);
+  }
+
+  void dispose() {
+    recordBehaviorSubject.close();
+    _onSaveButtonTappedStreamController.close();
+    _onDateTimeSelectedStreamController.close();
+    _onNoteChangedStreamController.close();
+    _onSaveCompleteStreamController.close();
+    _onDeleteButtonTappedStreamController.close();
+  }
+}
