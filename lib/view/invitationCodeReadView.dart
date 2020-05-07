241dacd08cf7dc3c477c339562ff9f488be0b2b3
松島勇貴 <macho.macho.dev@gmail.com>
2020-05-07 22:45:27 +0900

    No invitation code flow

--- lib/view/invitationCodeReadView.dart
+++ lib/view/invitationCodeReadView.dart
@@ -18,8 +18,6 @@ class _InvitationCodeReadViewState extends State<InvitationCodeReadView> {
 
   InvitationCodeReadViewModel _viewModel;
 
-  String invitationCodeJSON = 'aaa';
-
   @override
   void initState() {
     super.initState();
@@ -33,25 +31,45 @@ class _InvitationCodeReadViewState extends State<InvitationCodeReadView> {
 
   @override
   Widget build(BuildContext context) {
+    final _messageStyle = TextStyle(fontSize: 16);
+    final _readButtonStyle = TextStyle(fontSize: 16.0);
+    final _cancelButtonStyle = TextStyle(fontSize: 16.0);
+
     return Scaffold(
       appBar: AppBar(
+        leading: Container(),
         title: Text('招待コードの読み取り'),
       ),
-      body: Center(
+      body: Container(
+        padding: EdgeInsets.all(36),
         child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          crossAxisAlignment: CrossAxisAlignment.center,
           children: <Widget>[
             Text(
-              '招待コードを使って、他のユーザーとデータを共有しますか？'
+              '招待コードを読み込んで、\n他のユーザーとデータを共有しますか？',
+              textAlign: TextAlign.center,
+              style: _messageStyle,
+            ),
+            Container(
+                height: 48,
             ),
             RaisedButton(
               onPressed: _onUseInvitationCodeButtonTapped,
               child: Text(
-                '招待コードを使う',
-                style: TextStyle(fontSize: 16.0),
+                '招待コードを読み込む',
+                style: _readButtonStyle,
               ),
             ),
-            Text(
-              invitationCodeJSON,
+            FlatButton(
+              onPressed: () {
+                Navigator.pop(context);
+                widget.onInvitationCodeRead(null);
+              },
+              child: Text(
+                '招待コードを持っていない',
+                style: _cancelButtonStyle,
+              )
             ),
           ],
         ),
