f8c4576517cdd3f9003cb44c8f5dbdd684f7495a
松島勇貴 <macho.macho.dev@gmail.com>
2020-05-13 06:46:47 +0900

    ButtonOrderView's ListTile

--- lib/view/setting/buttonOrderView.dart
+++ lib/view/setting/buttonOrderView.dart
@@ -1,6 +1,7 @@
 
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
+import 'package:raisingchildrenrecord2/model/record.dart';
 import 'package:raisingchildrenrecord2/viewmodel/setting/buttonOrderViewModel.dart';
 
 class ButtonOrderView extends StatefulWidget {
@@ -35,20 +36,62 @@ class _ButtonOrderViewState extends State<ButtonOrderView> {
   }
 
   Widget _body() {
-    List<String> items = ['Alice', 'Bob', 'Charlie', 'D'];
-    return ReorderableListView(
+    return StreamBuilder(
+      stream: _viewModel.recordTypes,
+      builder: (context, snapshot) {
+        if (!snapshot.hasData) {
+          return Container();
+        }
 
-      onReorder: (oldIndex, newIndex) {
-
-        print("#### $oldIndex $newIndex");
+        final List<RecordType> recordTypes = snapshot.data;
+        return ReorderableListView(
+          onReorder: _onReorder,
+          children: <Widget>[
+            for (RecordType recordType in recordTypes)
+              _buildListTile(recordType),
+          ],
+        );
       },
-      children: <Widget>[
-        for (String item in items)
-          ListTile(
-            key: ValueKey(item),
-            title: Text(item),
-          ),
-      ]
     );
+  }
+
+  Widget _buildListTile(RecordType recordType) {
+    return Container(
+      key: Key(recordType.string),
+      padding: EdgeInsets.all(24),
+      child: Row(
+        crossAxisAlignment: CrossAxisAlignment.center,
+        children: <Widget>[
+          Container(
+            width: 36,
+            height: 36,
+            decoration: BoxDecoration(
+              shape: BoxShape.circle,
+              image: DecorationImage(
+                fit: BoxFit.fitHeight,
+                image: AssetImage(recordType.assetName),
+              )
+            ),
+          ),
+          Container(
+            height: 36,
+            width: 8
+          ),
+          Expanded(
+            child: Text(
+              recordType.localizedName,
+            ),
+          ),
+          Icon(
+            Icons.reorder,
+            size: 24,
+            color: Colors.grey,
+          )
+        ],
+      )
+    );
+  }
+
+  void _onReorder(int oldIndex, int newIndex) {
   }
 }
