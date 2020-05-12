817be750ee9e689da73d5e4cdf9e31a7deaad96c
松島勇貴 <macho.macho.dev@gmail.com>
2020-05-12 21:25:45 +0900

    Add ButtonOrderViewModel

---
+++ lib/viewmodel/setting/buttonOrderViewModel.dart
@@ -0,0 +1,45 @@
+
+import 'dart:async';
+
+import 'package:raisingchildrenrecord2/model/record.dart';
+import 'package:rxdart/rxdart.dart';
+import 'package:shared_preferences/shared_preferences.dart';
+
+class ButtonOrderViewModel {
+  final _onButtonOrderChangedStreamController = StreamController<List<String>>();
+  StreamSink<List<String>> get onButtonOrderChanged => _onButtonOrderChangedStreamController.sink;
+
+  final _buttonOrderBehaviorSubject = BehaviorSubject.seeded(List<String>());
+  Stream<List<String>> get buttonOrder => _buttonOrderBehaviorSubject.stream;
+
+  final _recordTypesBehaviorSubject = BehaviorSubject.seeded(List<RecordType>());
+  Stream<List<RecordType>> get recordTypes => _recordTypesBehaviorSubject.stream;
+
+  ButtonOrderViewModel() {
+    SharedPreferences.getInstance().then((sharedPreferences) {
+      List<String> recordOrder = sharedPreferences.getStringList('recordButtonOrder')
+        ?? RecordType.values.map((recordType) => recordType.string).toList();
+      _buttonOrderBehaviorSubject.sink.add(recordOrder);
+    });
+
+    _onButtonOrderChangedStreamController.stream.listen((buttonOrder) {
+      _buttonOrderBehaviorSubject.add(buttonOrder);
+      SharedPreferences.getInstance().then((sharedPreferences) {
+        sharedPreferences.setStringList('recordButtonOrder', buttonOrder);
+      });
+    });
+
+    _buttonOrderBehaviorSubject.stream.listen((buttonOrder) {
+      List<RecordType> recordTypes = buttonOrder
+          .map((recordTypeString) => RecordTypeExtension.fromString(recordTypeString))
+          .toList();
+      _recordTypesBehaviorSubject.add(recordTypes);
+    });
+  }
+
+  void dispose() {
+    _onButtonOrderChangedStreamController.close();
+    _buttonOrderBehaviorSubject.close();
+    _recordTypesBehaviorSubject.close();
+  }
+}
