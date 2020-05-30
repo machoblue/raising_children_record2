7bb23f5940ffe021fbe91479a69b0c3cb3c28f85
松島勇貴 <macho.macho.dev@gmail.com>
2020-05-30 14:37:53 +0900

    Add bodyTemperature, height and weight viewModel

---
+++ lib/viewmodel/record/heightRecordViewModel.dart
@@ -0,0 +1,27 @@
+
+import 'dart:async';
+import 'package:raisingchildrenrecord2/data/recordRepository.dart';
+import 'package:raisingchildrenrecord2/model/baby.dart';
+import 'package:raisingchildrenrecord2/model/record.dart';
+import 'package:raisingchildrenrecord2/model/user.dart';
+import 'package:raisingchildrenrecord2/viewmodel/record/baseRecordViewModel.dart';
+
+class HeightRecordViewModel extends BaseRecordViewModel<HeightRecord> {
+
+  Stream<double> get bodyTemperature => recordBehaviorSubject.stream.map((record) => record.height ?? 0);
+  final StreamController<double> _onHeightSelectedStreamController = StreamController<double>();
+  StreamSink<double> get onHeightSelected => _onHeightSelectedStreamController.sink;
+
+  HeightRecordViewModel(Record record, User user, Baby baby, RecordRepository recordRepository): super(record, user, baby, recordRepository) {
+
+    _onHeightSelectedStreamController.stream.listen((height) {
+      HeightRecord record = recordBehaviorSubject.value;
+      record.height = height;
+      recordBehaviorSubject.add(record);
+    });
+  }
+
+  void dispose() {
+    _onHeightSelectedStreamController.close();
+  }
+}
---
+++ lib/viewmodel/record/weightRecordViewModel.dart
@@ -0,0 +1,27 @@
+
+import 'dart:async';
+import 'package:raisingchildrenrecord2/data/recordRepository.dart';
+import 'package:raisingchildrenrecord2/model/baby.dart';
+import 'package:raisingchildrenrecord2/model/record.dart';
+import 'package:raisingchildrenrecord2/model/user.dart';
+import 'package:raisingchildrenrecord2/viewmodel/record/baseRecordViewModel.dart';
+
+class WeightRecordViewModel extends BaseRecordViewModel<WeightRecord> {
+
+  Stream<double> get bodyTemperature => recordBehaviorSubject.stream.map((record) => record.weight ?? 0);
+  final StreamController<double> _onWeightSelectedStreamController = StreamController<double>();
+  StreamSink<double> get onWeightSelected => _onWeightSelectedStreamController.sink;
+
+  WeightRecordViewModel(Record record, User user, Baby baby, RecordRepository recordRepository): super(record, user, baby, recordRepository) {
+
+    _onWeightSelectedStreamController.stream.listen((weight) {
+      WeightRecord record = recordBehaviorSubject.value;
+      record.weight = weight;
+      recordBehaviorSubject.add(record);
+    });
+  }
+
+  void dispose() {
+    _onWeightSelectedStreamController.close();
+  }
+}
