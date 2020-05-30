13a8ad2bfc563749200d8c0e7139b107fa40087b
松島勇貴 <macho.macho.dev@gmail.com>
2020-05-30 15:21:26 +0900

    Fix crash: There should be exactly one item with [DropdownButton]'s value: 50.0.

--- lib/view/homeView.dart
+++ lib/view/homeView.dart
@@ -244,7 +244,7 @@ class _HomeViewState extends BaseState<HomeView, HomeViewModel> with TickerProvi
           ),
         );
       case RecordType.weight:
-        WeightRecord record = WeightRecord.newInstance(DateTime.now(), null, user, 50.0);
+        WeightRecord record = WeightRecord.newInstance(DateTime.now(), null, user, 5.0);
         return Provider<WeightRecordViewModel>(
           create: (_) => WeightRecordViewModel(record, user, baby, FirestoreRecordRepository()),
           child: WeightRecordView(
