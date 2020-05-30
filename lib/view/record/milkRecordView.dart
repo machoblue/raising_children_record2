bbcdece828211b86e4be19f32b7ce2b2d9dd8b8e
松島勇貴 <macho.macho.dev@gmail.com>
2020-05-30 13:57:22 +0900

    Use simpleDropDownButton

--- lib/view/record/milkRecordView.dart
+++ lib/view/record/milkRecordView.dart
@@ -3,10 +3,10 @@ import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 import 'package:raisingchildrenrecord2/view/record/baseRecordView.dart';
+import 'package:raisingchildrenrecord2/view/widget/simpleDropdownButton.dart';
 import 'package:raisingchildrenrecord2/viewmodel/record/milkRecordViewModel.dart';
 
 class MilkRecordView extends BaseRecordView<MilkRecordViewModel> {
-  final _listItemFont = const TextStyle(fontSize: 20.0);
   MilkRecordViewModel viewModel;
 
   MilkRecordView({ Key key, isNew, onComplete }): super(key: key, isNew: isNew, onComplete: onComplete);
@@ -26,27 +26,10 @@ class MilkRecordView extends BaseRecordView<MilkRecordViewModel> {
               if (!snapshot.hasData) {
                 return Container();
               }
-              return DropdownButton<int>(
+              return SimpleDropdownButton<int>(
                 value: snapshot.data ?? 0,
-                items: List<int>.generate(36, (i) => i * 10).map((value) {
-                  return DropdownMenuItem<int>(
-                      value: value,
-                      child: Container(
-                          padding: EdgeInsets.fromLTRB(20, 0, 12, 0),
-                          child: Text(
-                            "$value",
-                            style: _listItemFont,
-                          )
-                      )
-                  );
-                }).toList(),
-                icon: Icon(Icons.expand_more),
-                iconSize: 24,
-                elevation: 16,
-                underline: Container(
-                  height: 2,
-                  color: Colors.black54,
-                ),
+                items: List<int>.generate(36, (i) => i * 10),
+                itemLabel: (value) => '$value',
                 onChanged: (int newValue) => viewModel.onAmountSelected.add(newValue),
               );
             }
--- lib/view/record/mothersMilkRecordView.dart
+++ lib/view/record/mothersMilkRecordView.dart
@@ -2,13 +2,12 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
-import 'package:raisingchildrenrecord2/l10n/l10n.dart';
 import 'package:raisingchildrenrecord2/view/record/baseRecordView.dart';
+import 'package:raisingchildrenrecord2/view/widget/simpleDropdownButton.dart';
 import 'package:raisingchildrenrecord2/viewmodel/record/mothersMilkRecordViewModel.dart';
 import 'package:intl/intl.dart';
 
 class MothersMilkRecordView extends BaseRecordView<MothersMilkRecordViewModel> {
-  final _listItemFont = const TextStyle(fontSize: 20.0);
   MothersMilkRecordViewModel viewModel;
 
   MothersMilkRecordView({ Key key, isNew, onComplete }): super(key: key, isNew: isNew, onComplete: onComplete);
@@ -38,28 +37,11 @@ class MothersMilkRecordView extends BaseRecordView<MothersMilkRecordViewModel> {
                   if (!snapshot.hasData) {
                     return Container();
                   }
-                  return DropdownButton<int>(
+                  return SimpleDropdownButton<int>(
                     value: snapshot.data ?? 0,
-                    items: List<int>.generate(60, (i) => i).map((value) {
-                      return DropdownMenuItem<int>(
-                          value: value,
-                          child: Container(
-                              padding: EdgeInsets.fromLTRB(20, 0, 12, 0),
-                              child: Text(
-                                "$value",
-                                style: _listItemFont,
-                              )
-                          )
-                      );
-                    }).toList(),
-                    icon: Icon(Icons.expand_more),
-                    iconSize: 24,
-                    elevation: 16,
-                    underline: Container(
-                      height: 2,
-                      color: Colors.black54,
-                    ),
-                    onChanged: (int newValue) => viewModel.onLeftMinutesSelected.add(newValue),
+                    items: List<int>.generate(60, (i) => i),
+                    itemLabel: (value) => '$value',
+                    onChanged: (newValue) => viewModel.onLeftMinutesSelected.add(newValue),
                   );
                 }
             ),
@@ -90,28 +72,11 @@ class MothersMilkRecordView extends BaseRecordView<MothersMilkRecordViewModel> {
                     print("### !snapshot.hasData");
                     return Container();
                   }
-                  return DropdownButton<int>(
+                  return SimpleDropdownButton<int>(
                     value: snapshot.data ?? 0,
-                    items: List<int>.generate(60, (i) => i).map((value) {
-                      return DropdownMenuItem<int>(
-                          value: value,
-                          child: Container(
-                              padding: EdgeInsets.fromLTRB(20, 0, 12, 0),
-                              child: Text(
-                                "$value",
-                                style: _listItemFont,
-                              )
-                          )
-                      );
-                    }).toList(),
-                    icon: Icon(Icons.expand_more),
-                    iconSize: 24,
-                    elevation: 16,
-                    underline: Container(
-                      height: 2,
-                      color: Colors.black54,
-                    ),
-                    onChanged: (int newValue) => viewModel.onRightMinutesSelected.add(newValue),
+                    items: List<int>.generate(60, (i) => i),
+                    itemLabel: (value) => '$value',
+                    onChanged: (newValue) => viewModel.onRightMinutesSelected.add(newValue),
                   );
                 }
             ),
