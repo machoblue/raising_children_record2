6fb2254d3f0a08ac126a03491bf78827f41c1acc
松島勇貴 <macho.macho.dev@gmail.com>
2020-06-02 21:51:26 +0900

    Add legends

--- lib/view/chart/milkChartView.dart
+++ lib/view/chart/milkChartView.dart
@@ -1,5 +1,7 @@
 
-import 'package:flutter/cupertino.dart';
+import 'dart:math';
+
+import 'package:flutter/material.dart';
 import 'package:raisingchildrenrecord2/l10n/l10n.dart';
 import 'package:raisingchildrenrecord2/view/baseState.dart';
 import 'package:raisingchildrenrecord2/view/widget/simpleSegmentedControl.dart';
@@ -23,27 +25,95 @@ class _MilkChartViewState extends BaseState<MilkChartView, MilkChartViewModel> {
           builder: (context, snapshot) {
             return SimpleSegmentedControl(
               currentIndex: snapshot.data,
-              labels: MilkViewTabItem.values.map((item) => item.getLabel(l10n)).toList(),
+              labels: MilkChartPeriod.values.map((item) => item.getLabel(l10n)).toList(),
               onSelect: viewModel.onSelected.add,
             );
           },
         ),
-        Container(),
+        Expanded(
+          child: Container(
+            child: Stack(
+              children: <Widget>[
+                CustomPaint(
+                  painter: MilkChartFramePainter(),
+                  child: Container(),
+                ),
+              ],
+            ),
+          ),
+        ),
       ],
     );
   }
 }
 
-enum MilkViewTabItem {
+enum MilkChartPeriod {
   oneWeek, threeWeeks, threeMonths,
 }
 
-extension MilkViewTabItemExtension on MilkViewTabItem {
+extension MilkChartPeriodExtension on MilkChartPeriod {
   String getLabel(L10n l10n) {
     switch (this) {
-      case MilkViewTabItem.oneWeek: return l10n.oneWeek;
-      case MilkViewTabItem.threeWeeks: return l10n.threeWeeks;
-      case MilkViewTabItem.threeMonths: return l10n.threeMonths;
+      case MilkChartPeriod.oneWeek: return l10n.oneWeek;
+      case MilkChartPeriod.threeWeeks: return l10n.threeWeeks;
+      case MilkChartPeriod.threeMonths: return l10n.threeMonths;
+      default: throw 'This line shouldn\'t be reached';
     }
   }
+}
+
+class MilkChartFramePainter extends CustomPainter {
+  @override
+  void paint(Canvas canvas, Size size) {
+    final double margin = min(size.width * 0.1, size.height * 0.1);
+
+    final double fontSize = 12;
+    final baseTextStyle = TextStyle(color: Colors.black, fontSize: fontSize);
+    final textSpan = TextSpan(
+      style: baseTextStyle,
+      children: <TextSpan>[
+        TextSpan(text: '●' ,style: TextStyle(color: Colors.yellow, fontSize: fontSize)),
+        TextSpan(text: 'ミルク '),
+        TextSpan(text: '●' ,style: TextStyle(color: Colors.pink, fontSize: fontSize)),
+        TextSpan(text: '母乳'),
+      ],
+    );
+
+    final textPainter = TextPainter(text: textSpan, textDirection: TextDirection.ltr);
+    textPainter.layout(minWidth: 0, maxWidth: size.width);
+    final double marginBottom = 6;
+    textPainter.paint(canvas, Offset(margin, margin - fontSize - marginBottom));
+
+    Path path = Path();
+    double x0 = margin;
+    double y0 = margin;
+    double x1 = x0;
+    double y1 = size.height - margin;
+    double x2 = size.width - margin;
+    double y2 = y1;
+    double x3 = x2;
+    double y3 = y0;
+    path.moveTo(x0, y0);
+    path.lineTo(x1, y1);
+    path.lineTo(x2, y2);
+    path.lineTo(x3, y3);
+
+    Paint paint = Paint()
+        ..color = Colors.black
+        ..strokeCap = StrokeCap.round
+        ..style = PaintingStyle.stroke
+        ..strokeWidth = 1;
+
+    canvas.drawPath(path, paint);
+  }
+
+  @override
+  bool shouldRepaint(CustomPainter oldDelegate) => false;
+}
+
+class MilkChartData {
+  final String name;
+  final Color color;
+  final Map<DateTime, double> dateToValue;
+  MilkChartData(this.name, this.color, this.dateToValue);
 }
