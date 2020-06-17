81c0cb68c673c4d3fdd21f7fbb842beba6968214
松島勇貴 <macho.macho.dev@gmail.com>
2020-06-18 07:22:19 +0900

    MHLWGrowthStaticsGroup

--- lib/viewmodel/chart/growthChartViewModel.dart
+++ lib/viewmodel/chart/growthChartViewModel.dart
@@ -16,3 +16,41 @@ class GrowthChartViewModel with ViewModelErrorHandler implements ViewModel {
     super.dispose();
   }
 }
+
+class GrowthStaticsGroup {
+  String get localizedName {}
+  GrowthStatics get heightGrowthStatics {}
+  GrowthStatics get weightGrowthStatics {}
+}
+
+class GrowthStatics {
+  final List<GrowthData> ceilDataList;
+  final List<GrowthData> floorDataList;
+  GrowthStatics({ this.ceilDataList, this.floorDataList });
+}
+
+class GrowthData {
+  final int month;
+  final double value;
+  GrowthData(this.month, this.value);
+}
+
+class MHLWGrowthStaticsGroup implements GrowthStaticsGroup { // 厚生労働省
+  String get localizedName => '厚生労働省'; // TODO: internationalize
+  GrowthStatics get heightGrowthStatics => _heightGrowthStatics;
+  GrowthStatics get weightGrowthStatics => _widthGrowthStatics;
+
+  final _heightGrowthStatics = GrowthStatics(
+    ceilDataList: [
+    ],
+    floorDataList: [
+    ],
+  );
+
+  final _widthGrowthStatics = GrowthStatics(
+    ceilDataList: [
+    ],
+    floorDataList: [
+    ],
+  );
+}
