b622fb388cbf5c2bdfaf563fc06caae0a1af0d64
松島勇貴 <macho.macho.dev@gmail.com>
2020-06-19 09:57:22 +0900

    GrowthChartViewModel Interface

--- lib/viewmodel/chart/growthChartViewModel.dart
+++ lib/viewmodel/chart/growthChartViewModel.dart
@@ -1,78 +1,92 @@
 
+import 'dart:async';
+
 import 'package:raisingchildrenrecord2/data/recordRepository.dart';
 import 'package:raisingchildrenrecord2/model/baby.dart';
 import 'package:raisingchildrenrecord2/viewmodel/baseViewModel.dart';
+import 'package:raisingchildrenrecord2/viewmodel/chart/growthStatisticsScheme.dart';
+import 'package:rxdart/rxdart.dart';
 
 class GrowthChartViewModel with ViewModelErrorHandler implements ViewModel {
 
   Stream<Baby> babyStream;
   RecordRepository recordRepository;
 
+  StreamSubscription _currentIndexSubscription;
+  StreamSubscription _babySubscription;
+
+  final _currentIndexBehaviorSubject = BehaviorSubject<int>.seeded(0);
+  Stream<int> get currentIndex => _currentIndexBehaviorSubject.stream;
+  StreamSink<int> get onSelected => _currentIndexBehaviorSubject.sink;
+
+  final _periodBehaviorSubject = BehaviorSubject<GrowthPeriod>.seeded(GrowthPeriod.oneYear);
+  Stream<GrowthPeriod> get period => _periodBehaviorSubject.stream;
+
+  final _statisticsDataStreamController = StreamController<GrowthStatisticsData>();
+  Stream<GrowthStatisticsData> get statisticsData => _statisticsDataStreamController.stream;
+
+  final _growthChartDataStreamController = StreamController<GrowthChartData>();
+  Stream<GrowthChartData> get growthChartData => _growthChartDataStreamController.stream;
+
   GrowthChartViewModel(this.babyStream, this.recordRepository) {
+    _babySubscription = babyStream.listen((baby) {
+      final growthStatisticsScheme = MHLWGrowthStatisticsScheme();
+      final growthStatisticsSet = baby.sex == Sex.female ? growthStatisticsScheme.femaleSet : growthStatisticsScheme.maleSet;
+    });
   }
 
   @override
   void dispose() {
     super.dispose();
+
+    _currentIndexSubscription.cancel();
+    _babySubscription.cancel();
+
+    _currentIndexBehaviorSubject.close();
+    _periodBehaviorSubject.close();
+    _statisticsDataStreamController.close();
+    _growthChartDataStreamController.close();
   }
 }
 
-class GrowthStaticsGroup {
-  String get localizedName {}
-  GrowthStaticsSet get maleSet {}
-  GrowthStaticsSet get femaileSet {}
+enum GrowthPeriod {
+  oneYear,
+  threeYears,
+  sixYears,
 }
 
-class GrowthStaticsSet {
-  final GrowthStatics heightStatics;
-  final GrowthStatics widthStatics;
-  GrowthStaticsSet({ this.heightStatics, this.widthStatics });
+extension GrowthPeriodExtension on GrowthPeriod {
+  int get months {
+    switch (this) {
+      case GrowthPeriod.oneYear:
+        return 12;
+      case GrowthPeriod.threeYears:
+        return 36;
+      case GrowthPeriod.sixYears:
+        return 72;
+      default:
+        throw 'This line should not be reached.';
+    }
+  }
+
+  static GrowthPeriod fromIndex(int index) {
+    switch (index) {
+      case 0:
+        return GrowthPeriod.oneYear;
+      case 1:
+        return GrowthPeriod.threeYears;
+      case 2:
+        return GrowthPeriod.sixYears;
+      default:
+        throw 'This line should not be reached.';
+    }
+  }
 }
 
-class GrowthStatics {
-  final List<GrowthData> ceilDataList;
-  final List<GrowthData> floorDataList;
-  GrowthStatics({ this.ceilDataList, this.floorDataList });
+class GrowthChartData {
+
 }
 
-class GrowthData {
-  final double month;
-  final double value;
-  GrowthData(this.month, this.value);
-}
+class GrowthStatisticsData {
 
-class MHLWGrowthStaticsGroup implements GrowthStaticsGroup { // 厚生労働省
-  String get localizedName => '厚生労働省'; // TODO: internationalize
-  GrowthStaticsSet get maleSet => _maleSet;
-  GrowthStaticsSet get femaileSet => _femaleSet;
-
-  final _maleSet = GrowthStaticsSet(
-    heightStatics: GrowthStatics(
-      ceilDataList: [
-      ],
-      floorDataList: [
-      ],
-    ),
-    widthStatics: GrowthStatics(
-      ceilDataList: [
-      ],
-      floorDataList: [
-      ],
-    ),
-  );
-
-  final _femaleSet = GrowthStaticsSet(
-    heightStatics: GrowthStatics(
-      ceilDataList: [
-      ],
-      floorDataList: [
-      ],
-    ),
-    widthStatics: GrowthStatics(
-      ceilDataList: [
-      ],
-      floorDataList: [
-      ],
-    ),
-  );
 }
---
+++ lib/viewmodel/chart/growthStatisticsScheme.dart
@@ -0,0 +1,62 @@
+import 'package:raisingchildrenrecord2/l10n/l10n.dart';
+
+class GrowthStatisticsScheme {
+  String localizedName(L10n l10n) {}
+  GrowthStatisticsSet get maleSet {}
+  GrowthStatisticsSet get femaleSet {}
+}
+
+class GrowthStatisticsSet {
+  final GrowthStatistics heightStatistics;
+  final GrowthStatistics widthStatistics;
+  GrowthStatisticsSet({ this.heightStatistics, this.widthStatistics });
+}
+
+class GrowthStatistics {
+  final List<GrowthData> ceilDataList;
+  final List<GrowthData> floorDataList;
+  GrowthStatistics({ this.ceilDataList, this.floorDataList });
+}
+
+class GrowthData {
+  final double month;
+  final double value;
+  GrowthData(this.month, this.value);
+}
+
+class MHLWGrowthStatisticsScheme implements GrowthStatisticsScheme { // 厚生労働省
+  GrowthStatisticsSet get maleSet => _maleSet;
+  GrowthStatisticsSet get femaleSet => _femaleSet;
+
+  String localizedName(L10n l10n) => '厚生労働省'; // TODO: localize
+
+  final _maleSet = GrowthStatisticsSet(
+    heightStatistics: GrowthStatistics(
+      ceilDataList: [
+      ],
+      floorDataList: [
+      ],
+    ),
+    widthStatistics: GrowthStatistics(
+      ceilDataList: [
+      ],
+      floorDataList: [
+      ],
+    ),
+  );
+
+  final _femaleSet = GrowthStatisticsSet(
+    heightStatistics: GrowthStatistics(
+      ceilDataList: [
+      ],
+      floorDataList: [
+      ],
+    ),
+    widthStatistics: GrowthStatistics(
+      ceilDataList: [
+      ],
+      floorDataList: [
+      ],
+    ),
+  );
+}
