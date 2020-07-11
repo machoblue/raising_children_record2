63a9a606815e23d591bb31e4625eb73b38b36716
matsuchiyo <macho.macho.dev@gmail.com>
2020-07-11 21:40:38 +0900

    Add registerView

---
+++ lib/viewmodel/registerViewModel.dart
@@ -0,0 +1,12 @@
+
+import 'package:raisingchildrenrecord2/viewmodel/baseViewModel.dart';
+
+class RegisterViewModel with ViewModelErrorHandler implements ViewModel {
+  RegisterViewModel() {
+  }
+
+  @override
+  void dispose() {
+    super.dispose();
+  }
+}
