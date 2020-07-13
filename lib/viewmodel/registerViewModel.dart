5c8c2df493712e0f70f66599c21972973aab2e4d
matsuchiyo <macho.macho.dev@gmail.com>
2020-07-13 10:06:18 +0900

    Sign in on registerView appear

--- lib/viewmodel/registerViewModel.dart
+++ lib/viewmodel/registerViewModel.dart
@@ -1,10 +1,14 @@
 
 import 'dart:async';
 
+import 'package:google_sign_in/google_sign_in.dart';
 import 'package:raisingchildrenrecord2/viewmodel/baseViewModel.dart';
 import 'package:rxdart/rxdart.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 
 class RegisterViewModel with ViewModelErrorHandler implements ViewModel {
+
+  final GoogleSignIn googleSignIn = GoogleSignIn();
 
   // Input
   final _onAppearStream = StreamController<void>();
@@ -29,13 +33,46 @@ class RegisterViewModel with ViewModelErrorHandler implements ViewModel {
   final _showIndicatorStreamController = BehaviorSubject.seeded(false);
   Stream<bool> get showIndicator => _showIndicatorStreamController.stream;
 
+  StreamSubscription _onAppearSubscription;
 
   RegisterViewModel() {
+    _onAppearSubscription = _onAppearStream.stream.listen((_) {
+      _showIndicatorStreamController.sink.add(true);
+
+      _isSignIn()
+        .then((isSignIn) {
+          if (isSignIn) {
+            _onSignInStreamController.sink.add(null);
+          }
+          _showIndicatorStreamController.sink.add(false);
+        })
+        .catchError(_handleError);
+    });
+  }
+
+  Future<bool> _isSignIn() {
+    return googleSignIn.isSignedIn().then((isSignIn) {
+      if (!isSignIn) {
+        return false;
+      }
+
+      return SharedPreferences.getInstance().then((sharedPreferences) {
+        final userId = sharedPreferences.getString("userId");
+        return userId?.isNotEmpty ?? false;
+      });
+    });
+  }
+
+  void _handleError(Object error) {
+    _showIndicatorStreamController.sink.add(false);
+    handleError(error);
   }
 
   @override
   void dispose() {
     super.dispose();
+
+    _onAppearSubscription.cancel();
 
     _onAppearStream.close();
     _onGoogleButtonTappedStream.close();
