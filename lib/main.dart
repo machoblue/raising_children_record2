030452fd726c5ba98bab7ba950dd0267900927e5
matsuchiyo <macho.macho.dev@gmail.com>
2020-07-22 09:58:30 +0900

    Implement registerView

--- lib/main.dart
+++ lib/main.dart
@@ -4,9 +4,12 @@ import 'package:provider/provider.dart';
 import 'package:raisingchildrenrecord2/data/babyRepository.dart';
 import 'package:raisingchildrenrecord2/data/userRepository.dart';
 import 'package:raisingchildrenrecord2/l10n/l10n.dart';
+import 'package:raisingchildrenrecord2/shared/authenticator.dart';
 import 'package:raisingchildrenrecord2/view/loginView.dart';
+import 'package:raisingchildrenrecord2/view/registerView.dart';
 import 'package:raisingchildrenrecord2/viewmodel/loginViewModel.dart';
 import 'package:firebase_crashlytics/firebase_crashlytics.dart';
+import 'package:raisingchildrenrecord2/viewmodel/registerViewModel.dart';
 
 void main() {
   FlutterError.onError = Crashlytics.instance.recordFlutterError;
@@ -23,9 +26,13 @@ class MyApp extends StatelessWidget {
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
+//      home: Provider(
+//        create: (_) => LoginViewModel(FirestoreUserRepository(), FirestoreBabyRepository()),
+//        child: LoginView(),
+//      ),
       home: Provider(
-        create: (_) => LoginViewModel(FirestoreUserRepository(), FirestoreBabyRepository()),
-        child: LoginView(),
+        create: (_) => RegisterViewModel(GoogleAuthenticator(), GuestAuthenticator(), FirestoreUserRepository(), FirestoreBabyRepository()),
+        child: RegisterView(),
       ),
       localizationsDelegates: [
         L10n.delegate,
--- lib/view/registerView.dart
+++ lib/view/registerView.dart
@@ -1,12 +1,18 @@
 
+import 'dart:async';
+
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 import 'package:raisingchildrenrecord2/data/babyRepository.dart';
 import 'package:raisingchildrenrecord2/data/userRepository.dart';
-import 'package:raisingchildrenrecord2/l10n/l10n.dart';
 import 'package:raisingchildrenrecord2/view/baseState.dart';
+import 'package:raisingchildrenrecord2/view/invitationCodeReadView.dart';
 import 'package:raisingchildrenrecord2/view/loginView.dart';
+import 'package:raisingchildrenrecord2/view/mainView.dart';
+import 'package:raisingchildrenrecord2/view/shared/utils.dart';
+import 'package:raisingchildrenrecord2/viewmodel/invitationCodeReadViewModel.dart';
 import 'package:raisingchildrenrecord2/viewmodel/loginViewModel.dart';
+import 'package:raisingchildrenrecord2/viewmodel/mainViewModel.dart';
 import 'package:raisingchildrenrecord2/viewmodel/registerViewModel.dart';
 
 class RegisterView extends StatefulWidget {
@@ -15,46 +21,131 @@ class RegisterView extends StatefulWidget {
 }
 
 class _RegisterViewState extends BaseState<RegisterView, RegisterViewModel>{
+
+  StreamSubscription _signInStreamSubscription;
+  StreamSubscription _needConfirmInvitationStreamSubscription;
+  StreamSubscription _alreadyRegisteredStreamSubscription;
+
+  @override
+  void initState() {
+    super.initState();
+
+    viewModel.onAppear.add(null);
+
+    _signInStreamSubscription = viewModel.onSignIn.listen((_) => _showMainView());
+    _needConfirmInvitationStreamSubscription = viewModel.needConfirmInvitationCode.listen((_) => _showInvitationReadView());
+    _alreadyRegisteredStreamSubscription = viewModel.alreadyRegistered.listen((_) => _showAlreadyRegisteredDialog());
+  }
+
   @override
   Widget build(BuildContext context) {
-    return Stack(
-      children: <Widget>[
-        Column(
-          children: <Widget>[
-            RaisedButton(
-              onPressed: () => viewModel.onGoogleButtonTapped.add(null),
-              child: Text(
-                L10n.of(context).loginButtonLabel,
-                style: TextStyle(fontSize: 16.0),
-              ),
+
+    return Scaffold(
+      appBar: AppBar(
+          title: Text("ユーザー登録")
+      ),
+      body: Stack(
+        children: <Widget>[
+          Center(
+            child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: <Widget>[
+                  Expanded(
+                    flex: 1,
+                    child: Center(
+                      child: Text(
+                          "赤ちゃん日記2",
+                          style: TextStyle(fontSize: 32, fontWeight: FontWeight.bold, color: Colors.blueGrey)
+                      ),
+                    ),
+                  ),
+                  Expanded(
+                    flex: 2,
+                    child: Column(
+                      children: <Widget>[
+                        RaisedButton(
+                          onPressed: () => viewModel.onGoogleButtonTapped.add(null),
+                          child: Text(
+                            "Googleで登録する",
+                            style: TextStyle(fontSize: 16.0),
+                          ),
+                        ),
+                        RaisedButton(
+                          onPressed: () => viewModel.onGuestButtonTapped.add(null),
+                          child: Text(
+                            "ゲストとして利用する",
+                            style: TextStyle(fontSize: 16.0),
+                          ),
+                        ),
+                        FlatButton(
+                          onPressed: () => _showLoginView(),
+                          child: Text(
+                            "登録済みのアカウントにログインする",
+                            style: TextStyle(fontSize: 16.0),
+                          ),
+                        )
+                      ],
+                    ),
+                  ),
+                ],
             ),
-            RaisedButton(
-              onPressed: () => viewModel.onGuestButtonTapped.add(null),
-              child: Text(
-                L10n.of(context).loginButtonLabel,
-                style: TextStyle(fontSize: 16.0),
-              ),
-            ),
-            FlatButton(
-              onPressed: () => _showLoginView(),
-              child: Text(
-                L10n.of(context).loginButtonLabel,
-                style: TextStyle(fontSize: 16.0),
-              ),
-            )
-          ]
-        ),
-        Center(
-          child: StreamBuilder(
-            stream: viewModel.showIndicator,
-            builder: (context, snapshot) {
-              return (snapshot.hasData && snapshot.data)
-                ? CircularProgressIndicator()
-                : Container();
-            },
           ),
+          Center(
+            child: StreamBuilder(
+              stream: viewModel.showIndicator,
+              builder: (context, snapshot) {
+                return (snapshot.hasData && snapshot.data)
+                    ? CircularProgressIndicator()
+                    : Container();
+              },
+            ),
+          )
+        ],
+      ),
+    );
+  }
+
+  @override
+  void dispose() {
+    super.dispose();
+    _signInStreamSubscription.cancel();
+    _needConfirmInvitationStreamSubscription.cancel();
+    _alreadyRegisteredStreamSubscription.cancel();
+  }
+
+  void _showMainView() {
+    Navigator.pushReplacement(
+      context,
+      MaterialPageRoute(
+        builder: (context) => Provider<MainViewModel>(
+          create: (_) => MainViewModel(FirestoreUserRepository(), FirestoreBabyRepository()),
+          child: MainView()
         )
-      ],
+      )
+    );
+  }
+
+  void _showInvitationReadView() {
+    Navigator.push(
+      context,
+      MaterialPageRoute(
+        builder: (context) => Provider<InvitationCodeReadViewModel>(
+          create: (_) => InvitationCodeReadViewModel(),
+          child: InvitationCodeReadView(onInvitationCodeRead: viewModel.onInvitationCodeRead.add,),
+        )
+      )
+    );
+  }
+
+  void _showAlreadyRegisteredDialog() {
+    showSimpleDialog(
+      context,
+      title: "確認",
+      content: "すでに登録済みです。ログインしますか？",
+      leftButtonTitle: "キャンセル",
+      rightButtonTitle: "OK",
+      onLeftButtonPressed: null,
+      onRightButtonPressed: () => _showMainView(),
     );
   }
 
